b746161f4797c08f4b9b10867a30af1d5db0646c
Amitpandey88 <70188972+Amitpandey88@users.noreply.github.com>
2023-03-28 15:14:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ function shortenUrl() {
     })
     .catch(error => console.log(error));
 }
+// JavaScript for pop-up
+window.onload = function() {
+  var popupWrapper = document.querySelector('.popup-wrapper');
+  var closePopup = document.querySelector('#close-popup');
+  
+  if(!sessionStorage.getItem('popupDisplayed')) {
+    popupWrapper.style.display = 'flex';
+    sessionStorage.setItem('popupDisplayed', 'true');
+  }
+  
+  closePopup.addEventListener('click', function() {
+    popupWrapper.style.display = 'none';
+  });
+}
